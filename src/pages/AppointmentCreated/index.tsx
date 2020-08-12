a71b94692d1ee68d6c8a09e787b1fc8607d26069
Adelar <adelarmello8@gmail.com>
2020-08-12 18:43:19 -0300

    Sucesso - Criando estrutura da tela

--- src/pages/AppointmentCreated/index.tsx
+++ src/pages/AppointmentCreated/index.tsx
@@ -1,7 +1,39 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { useCallback } from 'react';
+import Icon from 'react-native-vector-icons/Feather';
+
+import {
+  Container,
+  Title,
+  Description,
+  OkButton,
+  OkButtonText,
+} from './styles';
+import { useNavigation } from '@react-navigation/native';
 
 const AppointmentCreated: React.FC = () => {
-  return <View />;
+  const { reset } = useNavigation();
+
+  // Botão ok do agendamento marcado
+  const handleOkPressed = useCallback(() => {
+    reset({
+      routes: [
+        {
+          name: 'Dashboard', //Cria o agendamento e envia o usuário pro Dashboard
+        },
+      ],
+      index: 0,
+    });
+  }, [reset]);
+  return (
+    <Container>
+      <Icon name="check" size={80} color="#04d361" />
+      <Title> Agendamento concluido</Title>
+      <Description> Terça, dia 14 de março de 2020 as 12:00h </Description>
+      <OkButton onPress={handleOkPressed}>
+        <OkButtonText>Ok</OkButtonText>
+      </OkButton>
+    </Container>
+  );
 };
+
 export default AppointmentCreated;
